d1074a06fa474fc09aec3c3ae5aa05d4878d2ed7
Phellipe Lins <phellipelins@gmail.com>
2017-04-27 10:48:47 -0300

    unusable imported module

--- front-end/client/app/components/components.js
+++ front-end/client/app/components/components.js
@@ -2,13 +2,11 @@ import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
 import User from './user/user';
-import UserForm from './user/userForm/userForm';
 
 let componentModule = angular.module('app.components', [
   Home,
   About,
-  User,
-  UserForm
+  User
 ])
 
 .name;

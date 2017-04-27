5fcd6c14a64f2932b424794b64c726050cb7eaa4
Nattan Lucena <nattanlucena@gmail.com>
2017-04-27 09:39:35 -0300

    Fix development

--- api/config/config.js
+++ api/config/config.js
@@ -1,24 +1,31 @@
+/*
+    Module dependencies
+ */
 import fs from 'fs';
+import path from 'path';
 import dotenv from 'dotenv';
-import Logger from './logConfig';
 import bunyan from 'bunyan';
+import Logger from './logConfig';
 
 const logger = Logger.logger;
-
 const node_env = process.env.NODE_ENV;
-const file =  `.env.${node_env}`;
+const file = path.join(__dirname, '../env', `.env.${node_env}`);
 
 //Load env vars
 let envConfig = dotenv.parse(fs.readFileSync(file));
-
 for (let key in envConfig) {
     if (envConfig.hasOwnProperty(key)) {
         process.env[key] = envConfig[key];
     }
 }
 
-const config = {
 
+/**
+ *
+ * Load project config setup
+ *
+ */
+const config = {
     API_BASE_PATH: '/api',
 
     PORT: process.env.PORT || 3000,

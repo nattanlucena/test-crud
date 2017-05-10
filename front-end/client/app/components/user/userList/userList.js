9850c94535a5fc4de6d23256ed0b51a0116fc590
Marcello Dudk <marcello.dudk@gmail.com>
2017-05-10 11:09:39 -0300

    Correção de injeçãode dependecia duplicada

--- front-end/client/app/components/user/userList/userList.js
+++ front-end/client/app/components/user/userList/userList.js
@@ -11,10 +11,6 @@ let userListModule = angular.module('userList', [
 .component('userList', userListComponent)
 .factory('userListFactory', UserListFactory)
 .factory('userListService', userListService)
-
-
-.factory('UserListFactory', UserListFactory)
-
 .name;
 
 userListComponent.controller.$inject = ['userListFactory', 'userListService'];

49c670999bc81e5e05cecf693be3f14b049a5cb2
Nattan Lucena <nattanlucena@gmail.com>
2017-05-19 10:05:37 -0300

    Configuração no back-end e no Automation para testes de automação

---
+++ Automation/Helper/user-factory.js
@@ -0,0 +1,19 @@
+/**
+ * User factory
+ */
+module.exports = {
+    generateUsers : function() {
+        let users = [];
+        for (let i = 0; i < 100; i++) {
+            let user = {
+                "name": "user" + i,
+                "email": "user" + i + "@gmail.com",
+                "password": "12345" + i,
+                "cpf": "12345" + i
+            };
+            users.push(user);
+        }
+
+        return users;
+    }
+};
---
+++ Automation/Tests/_Setup/_setup.js
@@ -0,0 +1,8 @@
+let initDatabase = require('../../Helper/db_setup');
+
+
+describe('Automation tests setup', function() {
+    it('Init database', function() {
+       initDatabase();
+    });
+});

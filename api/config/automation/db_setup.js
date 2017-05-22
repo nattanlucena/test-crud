3b2f29942cbeff87490fb99e2b03139fe9e2be98
Nattan Lucena <nattanlucena@gmail.com>
2017-05-22 11:07:19 -0300

    Fix

--- api/config/automation/db_setup.js
+++ api/config/automation/db_setup.js
@@ -5,7 +5,7 @@ import { User } from '../../src/components/user';
 
 export const initAutomationDatabase = () => {
     // let userCollection = db.users;
-
+    console.log(new Date());
     if (mongoose.connection.db) {
         mongoose.connection.db.dropDatabase((err) => {
             if (err) {
@@ -17,8 +17,9 @@ export const initAutomationDatabase = () => {
     let users = userFactory.generateUsers();
     User.saveMany(users, function(err) {
         if (err) {
-            throw err;
+            console.log(err);
         }
+        console.log(new Date());
     });
 
 
